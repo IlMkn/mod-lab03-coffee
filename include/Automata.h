ca8d13bff8c37d59cd33ad52e1434d9c49589df5
IlMkn <91212388+IlMkn@users.noreply.github.com>
2022-03-26 15:22:37 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,17 +1,19 @@
+// Copyright 2022 GHA Test Team
+
 #pragma once
 
 #include <iostream>
 
-class Automata
-{
-private: enum STATES { Off, Wait, Accept, Check, Cook };
+class Automata {
+ private: 
+       enum STATES { Off, Wait, Accept, Check, Cook };
        int cash;
        std::string menu[10];
        int prices[10];
        STATES state;
        int chosenProduct;
 
-public: Automata();
+ public: Automata();
       int on();
       int off();
       int coin(int);
